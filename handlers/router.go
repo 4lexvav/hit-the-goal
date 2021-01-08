21fe60c8ccec4ba7cba02ed16980b1a3675b4ffa
4lexvav <4lexvav@gmail.com>
2021-01-08 15:56:00 +0200

    Remove test endpoint

--- handlers/router.go
+++ handlers/router.go
@@ -1,9 +1,6 @@
 package handlers
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/4lexvav/hit-the-goal/handlers/middlewares"
 	"github.com/4lexvav/hit-the-goal/handlers/projects"
 	"github.com/go-chi/chi"
@@ -15,11 +12,6 @@ func NewRouter() *chi.Mux {
 
 	r.Use(middleware.Recoverer)
 	r.Use(middlewares.RequestID)
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		binBody, _ := json.Marshal("Hello World")
-		w.Write(binBody)
-	})
 
 	r.Route("/projects", func(r chi.Router) {
 		r.Get("/", projects.GetList)
