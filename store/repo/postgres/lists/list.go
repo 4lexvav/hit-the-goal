86d43b945f872df1ade5e730539f5164ec97854c
4lexvav <4lexvav@gmail.com>
2021-01-20 23:41:21 +0200

    extend lists methods

--- store/repo/postgres/lists/list.go
+++ store/repo/postgres/lists/list.go
@@ -38,8 +38,19 @@ func (dao listsDAO) Get(projectID, size, page int) (lists []models.List, err err
 	return
 }
 
+func (dao listsDAO) GetByID(listID int) (list models.List, err error) {
+	err = dao.db.QueryRow("SELECT id, name, status, position, project_id, updated_at, created_at FROM lists WHERE id = $1", listID).
+		Scan(&list.ID, &list.Name, &list.Status, &list.Position, &list.ProjectID, &list.UpdatedAt, &list.CreatedAt)
+	return
+}
+
 func (dao listsDAO) GetMaxPosition(projectID int64) (position int16, err error) {
 	err = dao.db.QueryRow("SELECT MAX(position) FROM lists WHERE project_id = $1", projectID).Scan(&position)
+	return
+}
+
+func (dao listsDAO) GetListCount(projectID int) (count int, err error) {
+	err = dao.db.QueryRow("SELECT COUNT(*) FROM lists WHERE project_id = $1", projectID).Scan(&count)
 	return
 }
 
@@ -50,4 +61,27 @@ func (dao listsDAO) Insert(list models.List) (_ models.List, err error) {
 	}
 
 	return list, err
+}
+
+func (dao listsDAO) Update(list models.List) (_ models.List, err error) {
+	stmt := "UPDATE lists SET name = $1, status = $2, position = $3 WHERE id = $4 RETURNING updated_at"
+	if err = dao.db.QueryRow(stmt, list.Name, list.Status, list.Position, list.ID).Scan(&list.UpdatedAt); err != nil {
+		return models.List{}, err
+	}
+
+	return list, nil
+}
+
+func (dao listsDAO) Delete(id int) (err error) {
+	stmt, err := dao.db.Prepare("DELETE FROM lists WHERE id = $1")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
